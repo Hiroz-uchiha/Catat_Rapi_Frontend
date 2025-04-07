18aaf595c3c8afa38b7ba2d5bfacf4b2a6aa49de
kielFernandes <kielfrndes@gmail.com>
2025-04-07 13:18:32 +0700

    Perubahan frontend

--- src/Halaman/TambahGambar/Content.jsx
+++ src/Halaman/TambahGambar/Content.jsx
@@ -1,28 +1,18 @@
 import React from 'react'
-import Button from '../Todolist/Button'
-import {Rating} from "@mui/material";
 
-const Content = ({id, url,judul,isi}) => {
-      
-  
-    return (
-         <div className=''>
-                <div key={id} className='m-3 shadow-md p-2 w-3/5 mx-auto container flex h-72 bg-white'>
-                    <div className=' flex items-center w-1/4 justify-center p-2'>
-                        <img src={url} alt="gambar" className=' w-60 h-full' />
-                    </div>
-                    <div className='w-3/5 p-2 relative h-full'>
-                        <div className='space-y-1 '>
-                            <h1 className=' text-2xl bold'>{judul}</h1>
-                            <p>{isi}</p>
-                        </div>
-                        <div className='absolute right-0 bottom-2 w-full'>
-                            <Rating name="half-rating" defaultValue={5} precision={1} className=' bottom-5' size='large' />
-                            <Button className=" gap-6 "/>  
-                        </div>
-                    </div>
-                </div>
+const Content = ({ id, url, judul, isi }) => {
+  return (
+    <div className='flex justify-center my-4'>
+      <div key={id} className='shadow-md p-4 bg-white rounded'>
+        <div className='flex justify-center'>
+          <img src={url} alt="gambar" className='w-64 h-auto object-cover rounded' />
         </div>
+        <div className='mt-4 text-center'>
+          <h1 className='text-2xl font-bold'>{judul}</h1>
+          <p>{isi}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
